25825fab9ea4f72cfe24d56f0059dc9e30456e75
Jerry Lin <jerrylin9969@gmail.com>
2022-05-01 09:10:03 -0400

    first commit

---
+++ gui.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+
+import PySimpleGUI as sg
+
+import quickstart
+
+def get_data():
+    sg.theme('LightBlue3')  # Window Design
+    # Questions to ask user
+    layout = [[sg.Text('Wellness Log', font="bold")],
+              [sg.Text("1. How are you feeling?"), sg.Combo(values=[
+                  "Bad : (",
+                  "Okay : |",
+                  "Good : )"
+              ], size=50)],
+              [sg.Text("2. Have you eaten enough today?"), sg.Checkbox("")],
+              [sg.Text("3. Have you drank enough water?"), sg.Checkbox("")],
+              [sg.Text("4. Did you get enough sleep?"), sg.Checkbox("")],
+              [sg.Text("5. Did you leave the house today?"), sg.Checkbox("")],
+              [sg.Text("6. How has your day been from a scale of 1-10?"),
+               sg.InputText()],
+              [sg.Text("7. List 3 things that were good about today:"),
+               sg.InputText()],
+              [sg.Text("8. Note 1 thing you would change:"), sg.InputText()],
+              [sg.Text(size=(40, 1), key='-OUTPUT-')],
+              [sg.Button('Ok'), sg.Button('Cancel'), sg.Button("History")]]
+
+    history_layout = []
+
+    service = quickstart.start_authentication()
+    wellness_history = quickstart.get_document_data()
+    # Window Creation
+    window = sg.Window('Wellness Log', layout)
+    # Get all answers from the user
+    while True:
+        valid = True
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':  # if window is closed
+            # cancelled
+            break
+        if event == "Ok":
+            Mood, Eaten, Water, Snooze, House, = values[0], values[1], values[
+                2], values[3], values[4]
+            Scale, ThreeGood, OneChange = values[5], values[6], values[7]
+
+            try:
+                scaledummy = int(Scale)
+                if not 10 >= scaledummy >= 1:
+                    valid = False
+                    print(
+                        "Question 7 inputted incorrectly, make sure it's a "
+                        "number "
+                        "between 1-10!")
+            except ValueError:
+                valid = False
+                print(
+                    "Question 7 inputted incorrectly, make sure it's a number!")
+
+            if valid:
+                window['-OUTPUT-'].update("Daily Log Complete!")
+                todays_info = {
+                    "date": datetime.today().strftime('%Y-%m-%d'),
+                    "mood": Mood,
+                    "eaten": "Yes" if Eaten else "No",
+                    "water": "Yes" if Water else "No",
+                    "snooze": "Yes" if Snooze else "No",
+                    "house": "Yes" if House else "No",
+                    "scale": Scale,
+                    "threeGood": ThreeGood,
+                    "oneChange": OneChange
+                }
+                print(todays_info)
+                wellness_history.insert(0, todays_info)
+                quickstart.update_cloud_document(service, str(wellness_history))
+            if not valid:
+                continue
+        elif event == "History":
+            open_history_window(wellness_history)
+
+
+def open_history_window(wellness_history):
+    table_headings = [
+        "Date",
+        "Mood",
+        "Did you eat?",
+        "Did you drink water?",
+        "Sleep well",
+        "Left house",
+        "Rating",
+        "3 good things",
+        "What would you change"
+    ]
+    table_values = []
+    for point in wellness_history:
+        row = []
+        row.append(point["date"])
+        row.append(point["mood"])
+        row.append(point["eaten"])
+        row.append(point["water"])
+        row.append(point["snooze"])
+        row.append(point["house"])
+        row.append(point["scale"])
+        row.append(point["threeGood"])
+        row.append(point["oneChange"])
+        table_values.append(row)
+    layout = [[sg.Table(values=table_values, headings=table_headings)]]
+
+    window = sg.Window("History", layout, modal=True)
+    choice = None
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+get_data()
---
+++ quickstart.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+
+import json
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+          'https://www.googleapis.com/auth/drive.file']
+
+
+def start_authentication():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('docs', 'v1', credentials=creds)
+        update_local_document(service)
+        return service
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+
+def update_local_document(service):
+    # If document already exists
+    if os.path.exists('document_info.json'):
+        print("Attempting to access document_info.json...")
+        with open('document_info.json', 'r') as info:
+            doc_info = json.load(info)
+
+            try:
+                doc_id = doc_info["documentId"]
+            # If document_info.json is corrupted for whatever reason
+            except KeyError as error:
+                print("Document id not found.")
+                create_new_document(service)
+                return
+            info.close()
+
+        try:
+            doc = service.documents().get(documentId=doc_id).execute()
+            save_document_info(doc)
+            print("Successfully updated local data.")
+        except HttpError as error:
+            print(f"Document with id {doc_id} not found on Google Drive.")
+            create_new_document(service)
+
+    else:
+        print("Document not found.")
+        create_new_document(service)
+
+
+def update_cloud_document(service, new_info: str):
+    update_local_document(service)
+    with open('document_info.json', 'r') as info:
+        doc_info = json.load(info)
+        doc_id = doc_info["documentId"]
+        end_index = doc_info["body"]["content"][1]["endIndex"]
+        requests = []
+        if end_index > 2:
+            requests.append(
+                {"deleteContentRange": {
+                    "range": {
+                        "startIndex": 1,
+                        "endIndex": end_index - 1
+                    }
+                }})
+        requests.append(
+            {"insertText": {
+                "location": {
+                    "index": 1
+                },
+                "text": new_info
+            }})
+        service.documents().batchUpdate(documentId=doc_id, body={
+            "requests": requests
+        }).execute()
+        info.close()
+
+
+def get_document_data():
+    with open('document_info.json', 'r') as info:
+        doc_info = json.load(info)
+        doc_text = doc_info["body"]["content"][1]["paragraph"]["elements"] \
+            [0]["textRun"]["content"].rstrip("\n")
+        doc_text = doc_text.replace('\u201c', '"')
+        doc_text = doc_text.replace('\u201d', '"')
+        info.close()
+    return eval(f'{doc_text}')
+
+
+def create_new_document(service):
+    print("Creating new document...")
+    title = 'Deerhacks Wellness Data'
+    body = {
+        'title': title
+    }
+    doc = service.documents().create(body=body).execute()
+    print('Created document with title: {0}'.format(doc.get('title')))
+    save_document_info(doc)
+    update_cloud_document(service, "[]")
+
+
+def save_document_info(doc):
+    with open('document_info.json', 'w') as info:
+        json.dump(doc, info)
+
+
+if __name__ == '__main__':
+    start_authentication()
